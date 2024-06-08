a7bf4dbce3cf405986a7eb090f3f8f31cfc4d9fd
manolodalbo <manuel_dal_bo@brown.edu>
2024-06-07 21:25:23 -0400

    more testing

--- SpinTorch/run_model.py
+++ SpinTorch/run_model.py
@@ -1,52 +1,66 @@
 import torch
 import spintorch
 import pickle
+
 """Parameters"""
-dx = 50e-9      # discretization (m)
-dy = 50e-9      # discretization (m)
-dz = 20e-9      # discretization (m)
-nx = 100        # size x    (cells)
-ny = 100        # size y    (cells)
+dx = 50e-9  # discretization (m)
+dy = 50e-9  # discretization (m)
+dz = 20e-9  # discretization (m)
+nx = 100  # size x    (cells)
+ny = 100  # size y    (cells)
 
-Ms = 140e3      # saturation magnetization (A/m)
-B0 = 60e-3      # bias field (T)
-Bt = 1e-3       # excitation field amplitude (T)
+Ms = 140e3  # saturation magnetization (A/m)
+B0 = 60e-3  # bias field (T)
+Bt = 1e-3  # excitation field amplitude (T)
 
-dt = 20e-12     # timestep (s)
+dt = 20e-12  # timestep (s)
 batch_size = 32
-'''Directories'''
-basedir = 'focus_Ms/'
-plotdir = 'plots/' + basedir
+"""Directories"""
+basedir = "focus_Ms/"
+plotdir = "plots/" + basedir
 
-'''Geometry, sources, probes, model definitions'''
+"""Geometry, sources, probes, model definitions"""
 ### Here are three geometry modules initialized, just uncomment one of them to try:
 # Ms_CoPt = 723e3 # saturation magnetization of the nanomagnets (A/m)
 # r0, dr, dm, z_off = 15, 4, 2, 10  # starting pos, period, magnet size, z distance
 # rx, ry = int((nx-2*r0)/dr), int((ny-2*r0)/dr+1)
 # rho = torch.zeros((rx, ry))  # Design parameter array
-# geom = spintorch.WaveGeometryArray(rho, (nx, ny), (dx, dy, dz), Ms, B0, 
+# geom = spintorch.WaveGeometryArray(rho, (nx, ny), (dx, dy, dz), Ms, B0,
 #                                     r0, dr, dm, z_off, rx, ry, Ms_CoPt)
-B1 = 50e-3      # training field multiplier (T)
+B1 = 50e-3  # training field multiplier (T)
 geom = spintorch.WaveGeometryFreeForm((nx, ny), (dx, dy, dz), B0, B1, Ms)
 # geom = spintorch.WaveGeometryMs((nx, ny), (dx, dy, dz), Ms, B0)
-src = spintorch.WaveLineSource(10, 0, 10, ny-1, dim=2)
+src = spintorch.WaveLineSource(10, 0, 10, ny - 1, dim=2)
 probes = []
 Np = 2  # number of probes
 for p in range(Np):
-    probes.append(spintorch.WaveIntensityProbeDisk(nx-15, int(ny*(p+1)/(Np+1)), 2))
+    probes.append(
+        spintorch.WaveIntensityProbeDisk(nx - 15, int(ny * (p + 1) / (Np + 1)), 2)
+    )
 model = spintorch.MMSolver(geom, dt, batch_size, [src], probes)
-dev_name = 'cuda' if torch.cuda.is_available() else 'cpu'
+dev_name = "cuda" if torch.cuda.is_available() else "cpu"
 dev = torch.device(dev_name)  # 'cuda' or 'cpu'
-print('Running on', dev)
-model.load_state_dict(torch.load('C:/spins/Spins/models/focus_Ms/model_e2less_momentum.pt')['model_state_dict'])
-model.to(dev)   # sending model to GPU/CPU
-with open('C:\spins\data\data.p','rb') as data_file:
+print("Running on", dev)
+model.load_state_dict(
+    torch.load("C:/spins/Spins/models/focus_Ms/model_e25points0.04Bt.pt")[
+        "model_state_dict"
+    ]
+)
+model.to(dev)  # sending model to GPU/CPU
+with open("C:\spins\data\data.p", "rb") as data_file:
     data_dict = pickle.load(data_file)
-TEST_INPUTS = torch.tensor(data_dict['test_inputs']*Bt).unsqueeze(-1).to(dev)
+TEST_INPUTS = torch.tensor(data_dict["test_inputs"] * Bt).unsqueeze(-1).to(dev)
 print(TEST_INPUTS.shape)
-TEST_LABELS = torch.tensor(data_dict['test_labels'],dtype =torch.long).to(dev)
-accuracy = 0
+TEST_LABELS = data_dict["test_labels"].to(dev)
 with torch.no_grad():
-    output  = model(TEST_INPUTS[32:64])
-    accuracy += (output.argmax(dim=1) == TEST_LABELS[32:64]).float().mean()
-print(accuracy.item())
+    total_test_accuracy = 0
+    for i in range(TEST_INPUTS.shape[0] // 32 - 1):
+        test_outputs = model(TEST_INPUTS[i * 32 : (i + 1) * 32])
+        test_accuracy = (
+            (test_outputs.argmax(dim=-1) == TEST_LABELS[i * 32 : (i + 1) * 32])
+            .float()
+            .mean()
+        )
+        total_test_accuracy += test_accuracy
+    test_accuracy = total_test_accuracy / (i + 1)
+    print("Test Accuracy: %f" % (test_accuracy))
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import tensorflow as tf
 import torch
@@ -6,81 +5,132 @@ import pickle
 from torch import tensor
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import argparse
 
-def load_and_preprocess_data():
-    '''This is where we load in and preprocess our data! We load in the data 
-        for you but you'll need to flatten the images, normalize the values and 
+
+def parseArgs() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--points", type=int, default=3)
+    parser.add_argument("--pooling", type=bool, default=False)
+    parser.add_argument("--min_freq", type=float, default=0.4e9)
+    parser.add_argument("--max_freq", type=float, default=15e9)
+    parser.add_argument("--size", type=int, default=320)
+    parser.add_argument("--all_classes", type=bool, default=False)
+    args = parser.parse_args()
+    return args
+
+
+def load_and_preprocess_data(args: argparse.Namespace):
+    """This is where we load in and preprocess our data! We load in the data
+        for you but you'll need to flatten the images, normalize the values and
         convert the input images from numpy arrays into tensors
-    Return the preprocessed training and testing data and labels!'''
-    
-    #Load in the training and testing data from the MNIST dataset
-    (train_inputs, train_labels), (test_inputs, test_labels) = tf.keras.datasets.mnist.load_data()
-    train_inputs = train_inputs.reshape(-1,784)/255
-    test_inputs = test_inputs.reshape(-1,784)/255
-    refined_inputs  = []
+    Return the preprocessed training and testing data and labels!"""
+
+    # Load in the training and testing data from the MNIST dataset
+    (train_inputs, train_labels), (test_inputs, test_labels) = (
+        tf.keras.datasets.mnist.load_data()
+    )
+    print(train_inputs.shape)
+    if args.pooling:
+        train_inputs = pool(train_inputs)
+        test_inputs = pool(test_inputs)
+    dig_train_inputs = (
+        train_inputs.reshape(-1, train_inputs.shape[-1] * train_inputs.shape[-2]) / 255
+    )
+    dig_test_inputs = (
+        test_inputs.reshape(-1, test_inputs.shape[-1] * test_inputs.shape[-2]) / 255
+    )
+    refined_inputs = []
     refined_ouputs = []
-    # already_seen = set()
-    # i = 0
-    # while len(refined_inputs) < 10:
-    #     if train_labels[i] not in already_seen:
-    #         refined_inputs.append(train_inputs[i])
-    #         refined_ouputs.append(train_labels[i])
-    #         already_seen.add(train_labels[i])
-    #     i  = i + 1
-    ones = 0
-    zeros = 0
-    i = 0
-    while ones < 400 or zeros < 400:
-        if train_labels[i] == 1 and ones < 400:
-            refined_inputs.append(train_inputs[i])
-            refined_ouputs.append(train_labels[i])
-            ones = ones + 1
-        if train_labels[i] == 0 and zeros < 400:
-            refined_inputs.append(train_inputs[i])
-            refined_ouputs.append(train_labels[i])
-            zeros = zeros + 1
-        i += 1
-    i = 0
-    ones=0
-    zeros=0
+    if args.all_classes == False:
+        ones = 0
+        zeros = 0
+        i = 0
+        while ones < args.size // 2 or zeros < args.size // 2:
+            if train_labels[i] == 1 and ones < args.size // 2:
+                refined_inputs.append(dig_train_inputs[i])
+                refined_ouputs.append(train_labels[i])
+                ones = ones + 1
+            if train_labels[i] == 0 and zeros < args.size // 2:
+                refined_inputs.append(dig_train_inputs[i])
+                refined_ouputs.append(train_labels[i])
+                zeros = zeros + 1
+            i += 1
+    else:
+        refined_inputs = dig_train_inputs[0 : args.size]
+        refined_ouputs = train_labels[0 : args.size]
     new_test_inputs = []
     new_test_labels = []
-    number_of_samples_of_each_class = 160
-    while ones<number_of_samples_of_each_class or zeros<number_of_samples_of_each_class:
-        if test_labels[i] == 1 and ones < number_of_samples_of_each_class:
-            new_test_inputs.append(test_inputs[i])
-            new_test_labels .append(test_labels[i])
-            ones = ones + 1
-        if test_labels[i] == 0 and zeros < number_of_samples_of_each_class:
-            new_test_inputs.append(test_inputs[i])
-            new_test_labels.append(test_labels[i])
-            zeros = zeros + 1
-        i+=1
-        
-    train_inputs = fm(np.array(refined_inputs),0.1e9,5e9,3)
-    test_inputs = fm(np.array(new_test_inputs),0.1e9,5e9,3)
-    train_labels = tensor(refined_ouputs)
-    test_labels = tensor(new_test_labels)
-    with open(f'C:/spins/data/data.p', 'wb') as pickle_file:
-        pickle.dump(dict(train_inputs=train_inputs,train_labels=train_labels,test_inputs=test_inputs,test_labels=test_labels), pickle_file)
+    if args.all_classes == False:
+        i = 0
+        ones = 0
+        zeros = 0
+        number_of_samples_of_each_class = (
+            int(0.2 * args.size) // 2 if int(0.2 * args.size) >= 320 else 160
+        )
+        while (
+            ones < number_of_samples_of_each_class
+            or zeros < number_of_samples_of_each_class
+        ):
+            if test_labels[i] == 1 and ones < number_of_samples_of_each_class:
+                new_test_inputs.append(dig_test_inputs[i])
+                new_test_labels.append(test_labels[i])
+                ones = ones + 1
+            if test_labels[i] == 0 and zeros < number_of_samples_of_each_class:
+                new_test_inputs.append(dig_test_inputs[i])
+                new_test_labels.append(test_labels[i])
+                zeros = zeros + 1
+            i += 1
+    else:
+        testing_size = int(0.2 * args.size) if int(0.2 * args.size) >= 320 else 320
+        new_test_inputs = dig_test_inputs[0:testing_size]
+        new_test_labels = test_labels[0:testing_size]
+    train_inputs = fm(
+        np.array(refined_inputs), args.min_freq, args.max_freq, args.points
+    )
+    test_inputs = fm(
+        np.array(new_test_inputs), args.min_freq, args.max_freq, args.points
+    )
+    train_labels = tensor(refined_ouputs, dtype=torch.long)
+    test_labels = tensor(new_test_labels, dtype=torch.long)
+    print("train_inputs shape: ")
+    print(train_inputs.shape)
+    print("test_inputs shape: ")
+    print(test_inputs.shape)
+    print("train_labels shape: ")
+    print(train_labels.shape)
+    with open(f"C:/spins/data/data.p", "wb") as pickle_file:
+        pickle.dump(
+            dict(
+                train_inputs=train_inputs,
+                train_labels=train_labels,
+                test_inputs=test_inputs,
+                test_labels=test_labels,
+                dig_train_inputs=refined_inputs,
+                dig_test_inputs=new_test_inputs,
+            ),
+            pickle_file,
+        )
     print(f'Data has been dumped into {"C:/spins/data"}/data.p!')
-def fm(inputs: np.array, Fi: float, Ff: float,samples_per_point:int) -> np.array:
+
+
+def fm(inputs: np.array, Fi: float, Ff: float, samples_per_point: int) -> np.array:
     """
     Frequency modulate the input images.
-    
+
     Parameters:
         inputs (np.array): Array of shape (number of inputs, 784), where each row represents an image.
         Fi (float): Minimum frequency in Hz.
         Ff (float): Final frequency in Hz.
-    
+
     Returns:
         np.array: Frequency modulated waveforms for each input image.
     """
     points_per_input = samples_per_point
-    dt = 20e-12     # timestep (s)
+    dt = 20e-12  # timestep (s)
     timesteps = inputs.shape[1] * points_per_input
     t = np.arange(0, timesteps * dt, dt)  # time vector
-    modulated_wave = np.zeros((inputs.shape[0], timesteps),dtype="float32")
+    modulated_wave = np.zeros((inputs.shape[0], timesteps), dtype="float32")
     pbar = tqdm(inputs)
     for i in range(inputs.shape[0]):
         pbar.set_description(f"[({i+1}/{len(inputs)})] Processing images into waves")
@@ -89,22 +139,50 @@ def fm(inputs: np.array, Fi: float, Ff: float,samples_per_point:int) -> np.array
         for j, pixel_intensity in enumerate(inputs[i]):
             # Calculate the corresponding frequency for this pixel
             frequency = Fi + pixel_intensity * (Ff - Fi)
-            if j>0:
+            if j > 0:
                 phase = np.arcsin(prev)
                 if not pos_deriv:
                     phase = np.pi - phase
-                modulated_wave[i, points_per_input*j:points_per_input*(j+1)] = (0.5 + pixel_intensity * (2))*np.sin(2 * np.pi * frequency * t[1:points_per_input+1] + phase)
-                if np.cos(2*np.pi*frequency*t[points_per_input] + phase) > 0:
-                    pos_deriv=True
+                modulated_wave[i, points_per_input * j : points_per_input * (j + 1)] = (
+                    0.5 + pixel_intensity * (4.5)
+                ) * np.sin(2 * np.pi * frequency * t[1 : points_per_input + 1] + phase)
+                if np.cos(2 * np.pi * frequency * t[points_per_input] + phase) > 0:
+                    pos_deriv = True
                 else:
                     pos_deriv = False
-                prev = np.sin(2* np.pi * frequency * t[points_per_input] + phase)
+                prev = np.sin(2 * np.pi * frequency * t[points_per_input] + phase)
             else:
-                modulated_wave[i,points_per_input*j:points_per_input*(j+1)] = (0.5 + pixel_intensity * (2))*np.sin(2*np.pi*frequency*t[0:points_per_input])
-                if np.cos(2*np.pi*frequency*t[points_per_input-1]) > 0:
-                    pos_deriv=True
+                modulated_wave[i, points_per_input * j : points_per_input * (j + 1)] = (
+                    0.5 + pixel_intensity * (4.5)
+                ) * np.sin(2 * np.pi * frequency * t[0:points_per_input])
+                if np.cos(2 * np.pi * frequency * t[points_per_input - 1]) > 0:
+                    pos_deriv = True
                 else:
                     pos_deriv = False
-                prev = np.sin(2* np.pi * frequency *t[points_per_input-1])
+                prev = np.sin(2 * np.pi * frequency * t[points_per_input - 1])
     return modulated_wave
-load_and_preprocess_data()
+
+
+def pool(inputs: np.array):
+    """Performs average pooling on image, effectively cutting down the resolution. For
+    mnist this means going from 28 by 28 to 14 by 14"""
+    inputs = tensor(inputs, dtype=torch.float32)
+    inputs = inputs.unsqueeze(1)
+    # the output width and heigh is governed by the following equation assuming no padding:
+    # w_f = (w_i + filter_width)/stride and the same for heigh
+    pooling_layer = torch.nn.AvgPool2d(kernel_size=(2, 2), stride=2, padding=0)
+    pooled = pooling_layer(inputs).squeeze()
+    to_return = pooled.numpy()
+    return to_return
+
+
+def show_image(image: np.array):
+    """
+    Used to show mnist image"""
+    plt.imshow(image, cmap="gray")
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    load_and_preprocess_data(parseArgs())
