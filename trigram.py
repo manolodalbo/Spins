320eccffb30815c90960a3969868498aa76f0db2
manolodalbo <manuel_dal_bo@brown.edu>
2024-07-19 14:44:44 -0400

    all set up to run analysis

--- trigram.py
+++ trigram.py
@@ -15,8 +15,29 @@ class MyTrigram(nn.Module):
     def forward(self,inputs):
         wave_inputs = turn_into_wave(inputs,self.embedding_matrix)
         film_output = self.film_RNN(wave_inputs)
-        print(film_output.shape)
-        print("called")
+        #it might be worthwhile to crop the output to only once both inputs have gone
+        # it might also make sense to just weight later outputs more strongly. linearly increasing weights?
+        film_output = film_output.sum(dim=-1) #need to do some sort of normalization here to get it in the general range [-5,5]. Need to do analysis on what typical std and mean are to normalize
+        distances = self.euclidean_distance(film_output) #batch_size x vocab_size
+        probs = 1 - distances/distances.sum(dim=-1).unsqueeze(0)
+        return probs
+    def euclidean_distance(self,outputs):
+        """"
+        Note: not actually euclidean distance because I don't take the square root after adding
+        Should takes in the the output after passing through film and normalized(shape batch_size x embed_size) and compares
+        with the embedding matrix. Returns batch_size x vocab_size
+        """
+        distances = outputs.unsqueeze(1) - self.embedding_matrix.unsqueeze(0) #bs X vocab_size X embed_size
+        distances = (distances ** 2).sum(dim = -1) #essentially gets mean squared error(this may not be necessary)
+
+        #there are two ways to do this:
+        # the first is to use the distances to compute logits and the second is to directly
+        # use the mean squared error to compute the loss, however, the embeddings would have
+        # to be fixed because or else all the embeddings could be the same and would still result in 0 loss every time
+        # i dont think MSE will work in this case without computing probs later
+        # useful bc it gets rid of negatives and punishes terrible guesses
+        return distances
+
 def perplexity(labels,preds):
     loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
     calculate_categorical = loss_fn(preds, labels)
